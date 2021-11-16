d9dbe6dd61f2793b9df7c650b08f80085579d002
Burak Çelikkiran <burak.celikkiran@hotmail.com>
2021-11-16 16:24:15 +0300

    nova commit

---
+++ nova/resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<p class="mt-8 text-center text-xs text-80">
+    <a href="https://ko.com.tr" class="text-primary dim no-underline">Kapital Online</a>
+    <span class="px-1">&middot;</span>
+    All Rights Reserved &copy; {{ date('Y') }}
+</p>
